d7c32f736d2ca7cb9cfd4039829f7f8473a64686
Fshimi Hawlk <fshimi.hawlk@gmail.com>
2026-01-02 00:40:08 +0100

    added more prefab / changed the name of color array / modified the default positions

--- blockBlast/src/utils/globals.c
+++ blockBlast/src/utils/globals.c
@@ -1,15 +1,16 @@
 #include "utils/globals.h"
+#include "utils/userTypes.h"
 
 Rectangle windowRect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
 Font      fonts[MAX_FONT_COUNT] = {0};
 
 const f32Vector2 defaultPositions[3] = {
-    {.x = WINDOW_WIDTH * 20 / 100.0f, .y = WINDOW_HEIGHT * 85 / 100.0f},
-    {.x = WINDOW_WIDTH * 50 / 100.0f, .y = WINDOW_HEIGHT * 85 / 100.0f},
-    {.x = WINDOW_WIDTH * 80 / 100.0f, .y = WINDOW_HEIGHT * 85 / 100.0f},
+    {.x = WINDOW_WIDTH * 20 / 100.0f, .y = WINDOW_HEIGHT * 75 / 100.0f},
+    {.x = WINDOW_WIDTH * 50 / 100.0f, .y = WINDOW_HEIGHT * 75 / 100.0f},
+    {.x = WINDOW_WIDTH * 80 / 100.0f, .y = WINDOW_HEIGHT * 75 / 100.0f},
 };
 
-const color32 cellColors[_cellColorCount] = {
+const color32 blockColors[_blockColorCount] = {
     { .r = 254, .g =  40, .b =  44, .a = 225 }, // Red
     { .r = 232, .g = 116, .b =  33, .a = 225 }, // Orange
     { .r = 242, .g = 187, .b =  58, .a = 255 }, // Yellow
@@ -20,22 +21,89 @@ const color32 cellColors[_cellColorCount] = {
     { .r = 255, .b = 109, .g = 194, .a = 255 }, // Pink
 };
 
-const PrefabData_St prefabs[] = {
-    { .cellCount = 1, .DOC = 0, .canMirror = false, .relPos = { {0, 0} } }, // unit (1x1)
-    { .cellCount = 2, .DOC = 2, .canMirror = false, .relPos = { {0, 0}, {0, 1} } }, // Vertical (1x2)
-    { .cellCount = 3, .DOC = 2, .canMirror = false, .relPos = { {0, 0}, {0, 1}, {0, 2} } }, // Vertical (1x3)
-    { .cellCount = 3, .DOC = 4, .canMirror = false, .relPos = { {0, 0}, {0, 1}, {1, 0} } }, // L-shape
-    { .cellCount = 4, .DOC = 2, .canMirror = false, .relPos = { {0, 0}, {0, 1}, {0, 2}, {0, 3} } }, // Vertical (1x4)
-    { .cellCount = 4, .DOC = 0, .canMirror = false, .relPos = { {0, 0}, {0, 1}, {1, 0}, {1, 1} } }, // 2x2-shape
-    { .cellCount = 4, .DOC = 4, .canMirror = false, .relPos = { {0, 0}, {1, 0}, {2, 0}, {1, 1} } }, // T-shape
-    { .cellCount = 4, .DOC = 4, .canMirror = true,  .relPos = { {0, 0}, {1, 0}, {0, 1}, {0, 2} } }, // L-shape
-    { .cellCount = 4, .DOC = 2, .canMirror = true,  .relPos = { {0, 0}, {0, 1}, {1, 1}, {1, 2} } }, // Z-shape
-    { .cellCount = 5, .DOC = 2, .canMirror = false, .relPos = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4} } }, // Vertical (1x5)
-    { .cellCount = 5, .DOC = 0, .canMirror = false, .relPos = { {0, 1}, {1, 0}, {1, 1}, {1, 2}, {2, 1} } }, // Plus-shape
-    { .cellCount = 5, .DOC = 4, .canMirror = false, .relPos = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {2, 0} } }, // L-shape
-    { .cellCount = 6, .DOC = 2, .canMirror = false, .relPos = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5} } }, // Vertical (1x6)
-    { .cellCount = 6, .DOC = 2, .canMirror = false, .relPos = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2} } }, // 2x3 rectangle
-    { .cellCount = 9, .DOC = 0, .canMirror = false, .relPos = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}, {2, 0}, {2, 1}, {2, 2} } }, // 2x3 rectangle
+const Prefab_St prefabs[] = {
+    [PREFAB_1x1]  = { .blockCount = 1, .orientations = -1, .offsets = { {0, 0} } },
+    [PREFAB_2x2]  = { .blockCount = 4, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {1, 0}, {1, 1} } },
+    [PREFAB_2x3]  = { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2} } },
+    [PREFAB_3x3]  = { .blockCount = 9, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}, {2, 0}, {2, 1}, {2, 2} } },
+    [PREFAB_1x2]  = { .blockCount = 2, .orientations = -1, .offsets = { {0, 0}, {0, 1} } },
+    [PREFAB_1x3]  = { .blockCount = 3, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2} } },
+    [PREFAB_1x4]  = { .blockCount = 4, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3} } },
+    [PREFAB_1x5]  = { .blockCount = 5, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4} } },
+    [PREFAB_1x6]  = { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5} } },
+    [PREFAB_L3]   = { .blockCount = 3, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {1, 0} } },
+    [PREFAB_L4]   = { .blockCount = 4, .orientations = -1, .offsets = { {0, 0}, {1, 0}, {0, 1}, {0, 2} } },
+    [PREFAB_L5]   = { .blockCount = 5, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {2, 0} } },
+    [PREFAB_T]    = { .blockCount = 4, .orientations = -1, .offsets = { {0, 0}, {1, 0}, {2, 0}, {1, 1} } },
+    [PREFAB_Z]    = { .blockCount = 4, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {1, 1}, {1, 2} } },
+    [PREFAB_PLUS] = { .blockCount = 5, .orientations = -1, .offsets = { {0, 1}, {1, 0}, {1, 1}, {1, 2}, {2, 1} } },
+    [_prefabNameCount] = { .blockCount = 5, .orientations = -1, .offsets = { {1, 0}, {0, 1}, {1, 1}, {1, 2}, {2, 2} } },
+    { .blockCount = 5, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 1}, {1, 2} } },
+    { .blockCount = 5, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 3} } },
+    { .blockCount = 5, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {1, 1}, {1, 2}, {1, 3} } },
+    { .blockCount = 5, .orientations = -1, .offsets = { {0, 0}, {1, 0}, {1, 1}, {0, 2}, {1, 2} } },
+    { .blockCount = 5, .orientations = -1, .offsets = { {0, 0}, {1, 0}, {1, 1}, {2, 1}, {2, 2} } },
+    { .blockCount = 5, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 2} } },
+    { .blockCount = 5, .orientations = -1, .offsets = { {0, 0}, {1, 0}, {1, 1}, {1, 2}, {2, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 0} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 0}, {2, 0} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 1}, {1, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 1}, {2, 0} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 1}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 1}, {1, 2}, {0, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 1}, {1, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 1}, {1, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {2, 0} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {1, 0}, {0, 2}, {0, 3}, {1, 1}, {1, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {1, 1}, {1, 2}, {0, 2}, {0, 3}, {2, 0} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {1, 1}, {1, 2}, {0, 2}, {0, 3}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {2, 0}, {1, 1}, {1, 2}, {0, 2}, {0, 3}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {2, 0}, {1, 1}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {1, 1}, {0, 2}, {0, 3}, {1, 2}, {2, 2} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {0, 1}, {0, 2}, {1, 1}, {2, 0}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 1}, {2, 1}, {3, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 1}, {2, 1}, {3, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {0, 1}, {0, 2}, {1, 1}, {2, 1}, {3, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {2, 0}, {0, 1}, {0, 2}, {1, 1}, {2, 1}, {3, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {3, 0}, {0, 1}, {0, 2}, {1, 1}, {2, 1}, {3, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {1, 0}, {2, 0}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 3} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {0, 1}, {0, 2}, {1, 1}, {1, 2}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {0, 0}, {1, 0}, {0, 2}, {1, 1}, {1, 2}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {1, 1}, {0, 2}, {1, 2}, {1, 3}, {2, 1} } },
+    { .blockCount = 6, .orientations = -1, .offsets = { {1, 0}, {1, 1}, {0, 2}, {1, 2}, {1, 3}, {2, 2} } },
 };
 
 const u32 prefabCount = sizeof(prefabs) / sizeof(*prefabs);
+
+
+// Map_St maps[] = {
+//     {
+//         .board = {0},
+//         .columnCount = 8,
+//         .rowCount = 8,
+//     },
+//     {
+//         .board = {
+//             -1, -1,  0,  0,  0,  0,  0,  0, -1, -1,
+//             -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
+//              0,  0,  1,  0, -1, -1,  0,  1,  0,  0,
+//              0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+//              0,  0, -1,  0,  0,  0,  0, -1,  0,  0,
+//              0,  0, -1,  0,  0,  0,  0, -1,  0,  0,
+//              0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+//              0,  0,  1,  0, -1, -1,  0,  1,  0,  0,
+//             -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
+//             -1, -1,  0,  0,  0,  0,  0,  0, -1, -1,
+//         },
+//         .columnCount = 10,
+//         .rowCount = 10,
+//     },
+// };
