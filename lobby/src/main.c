eb4871c1e98b98036f0bf5b05026dae6bb5f59d6
LeandreB8 <leandre.baudet72@gmail.com>
2026-01-14 13:18:49 +0100

    start coding

---
+++ lobby/src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "config.h"
+
+#include "raylib.h"
+
+int main() {
+    SetTraceLogLevel(LOG_WARNING);
+
+    InitWindow(screenWidth, screenHeight, "Basic Raylib Window");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        
+        EndDrawing();
+    }
+    
+    CloseWindow();
+    return 0;
+}
