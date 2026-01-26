6c099663b4349227756766d762d2478b8d2cc4da
LeandreB8 <leandre.baudet72@gmail.com>
2026-01-26 10:19:11 +0100

    - refactor: fragmented main into dedicated files - feat: added coyote time - feat: added multi-jump capabilities - feat: added player skins - feat: added player skin menu to switch player's skin - fix: multiple collision bug fixed

---
+++ lobby/src/global.c
@@ -0,0 +1,42 @@
+#include "global.h"
+#include "config.h"
+
+Platform_st platforms[] = {
+    // Sol principal
+    {{-1000, 500, 2000, 1000}, GREEN, 0},
+
+    // Murs
+    {{-1000, 0, 500, 500}, BLACK, 0},
+    {{500, 0, 500, 500}, BLACK, 0},
+
+    // Platformes
+    {{-350, 400, 100, 30}, BROWN, 0.5},
+    {{250, 400, 100, 30}, BROWN, 0.5},
+    {{-200, 300, 100, 30}, BROWN, 0.5},
+    {{100, 300, 100, 30}, BROWN, 0.5},
+    {{-50, 200, 100, 30}, BROWN, 0.5},
+    {{-350, 150, 100, 30}, BROWN, 0.5},
+    {{250, 150, 100, 30}, BROWN, 0.5},
+};
+int platformCount = sizeof(platforms) / sizeof(platforms[0]);
+
+Texture2D playerTextures[2] = {0}; // à revoir pour une gestion dynamique
+int playerTextureCount = 0;
+
+Rectangle defaultPlayerTextureRect = {
+    20,
+    60,
+    50,
+    50
+};
+
+Rectangle skinButtonRect = {
+    WINDOW_WIDTH - 70,
+    WINDOW_HEIGHT / 2 - 25,
+    50,
+    50
+};
+
+bool isTextureMenuOpen = false;
+
+Texture2D logoSkinButton;
