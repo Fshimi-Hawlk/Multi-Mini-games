5ac6f2babc5753f0c37c5c90e63f2826880e211d
Fshimi Hawlk <fshimi.hawlk@gmail.com>
2026-02-07 23:59:24 +0100

    - docs: Removed unnecessary info about the root-level Makefile - feat: Use of the tetris API into the lobby as a quick example test - style: Fix typo - refactor: Removed/Added unnecessary/necessary include. - style: Rework tetris UI graphics

--- lobby/src/main.c
+++ lobby/src/main.c
@@ -5,67 +5,140 @@
 
 #include "utils/globals.h"
 
-Camera2D cam = {0};
+#include "APIs/tetrisAPI.h"
+
+typedef enum {
+    GAME_SCENE_LOBBY,
+    GAME_SCENE_TETRIS,
+} GameScene_Et;
+
+typedef struct {
+    bool running;
+} Game_St;
+
+Player_st player = {
+    .position = {0, 250},
+    .radius = 20,
+
+    .texture = NULL,
+    .angle = 0,
+
+    .velocity = {0, 0},
+
+    .onGround = false,
+    .nbJumps = 0,
+
+    .coyoteTime = 0.1f,
+    .coyoteTimer = 0.1f,
+
+    .jumpBuffer = 0.2f
+};
+
+Camera2D cam = {
+    .offset = (Vector2) {WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT / 2.0f},
+    .zoom = 1.0f,
+};
+
+Rectangle tetrisHitbox = {
+    .x = 600,
+    .y = -150,
+    .width = 75,
+    .height = 75
+};
+
+GameScene_Et currentScene = GAME_SCENE_LOBBY;
+bool needGameInit = false;
+bool gameHitGracePeriodActive = false;
+
+bool isGameRunning(const Game_St* game) {
+    return game->running;
+}
+
+void lobby_gameLoop(float dt) {
+    updatePlayer(&player, platforms, platformCount, dt);
+
+    cam.target = player.position;
+
+    toggleSkinMenu();
+
+    if (isTextureMenuOpen) {
+        choosePlayerTexture(&player);
+    }
+
+    if (CheckCollisionCircleRec(player.position, player.radius, tetrisHitbox)) {
+        if (!gameHitGracePeriodActive) {
+            currentScene = GAME_SCENE_TETRIS;
+            needGameInit = true;
+            gameHitGracePeriodActive = true;
+        }
+    } else if (gameHitGracePeriodActive) {
+        gameHitGracePeriodActive = false;
+    }
+
+    BeginDrawing(); {
+        ClearBackground(RAYWHITE);
+        BeginMode2D(cam); {
+            DrawCircle(0, 0, 10, RED);
+
+            drawPlayer(&player);
+            drawPlatforms(platforms, platformCount);
+
+            DrawRectangleRec(tetrisHitbox, RED);
+        } EndMode2D();
+
+        DrawText("Multi-Mini-Games", WINDOW_WIDTH / 2 - MeasureText("Multi-Mini-Games", 20) / 2, 20, 20, PURPLE);
+        drawSkinButton();
+        
+        if (isTextureMenuOpen) {
+            drawMenuTextures();
+        }
+    } EndDrawing();
+}
 
 int main(void) {
     SetTraceLogLevel(LOG_WARNING);
 
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Basic Raylib Window");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
-    logoSkinButton = LoadTexture("assets/logoSkin.png");
+    logoSkinButton = LoadTexture(IMAGES_PATH "logoSkin.png");
 
-    playerTextures[0] = LoadTexture("assets/earth.png"); playerTextureCount++;
-    playerTextures[1] = LoadTexture("assets/trollFace.png"); playerTextureCount++;
+    playerTextures[0] = LoadTexture(IMAGES_PATH "earth.png"); playerTextureCount++;
+    playerTextures[1] = LoadTexture(IMAGES_PATH "trollFace.png"); playerTextureCount++;
     
-    Player_st player = {
-        .position = {0, 300},
-        .radius = 20,
-
-        .texture = NULL,
-        .angle = 0,
-
-        .velocity = {0, 0},
-
-        .onGround = false,
-        .nbJumps = 0,
-
-        .coyoteTime = 0.1f,
-        .coyoteTimer = 0.1f,
-
-        .jumpBuffer = 0.2f
-    };
-
     cam.target = player.position;
-    cam.offset = (Vector2) {WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT / 2.0f};
-    cam.zoom = 1.0f;
+
+    TetrisGame_St* tetrisGame = NULL;
 
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
 
-        updatePlayer(&player, platforms, platformCount, dt);
+        switch (currentScene) {
+            case GAME_SCENE_LOBBY: {
+                lobby_gameLoop(dt);
+            } break;
 
-        cam.target = player.position;
+            case GAME_SCENE_TETRIS: {
+                if (needGameInit) {
+                    tetrisGame = tetris_initGame();
+                    needGameInit = false;
+                }
 
-        toggleSkinMenu();
+                tetris_gameLoop(tetrisGame);
 
-        if (isTextureMenuOpen) {
-            choosePlayerTexture(&player);
-        }
+                if (!isGameRunning((Game_St *) tetrisGame)) {
+                    tetris_freeGame(&tetrisGame);
+                    currentScene = GAME_SCENE_LOBBY;
+                }
+            } break;
 
-        BeginDrawing(); {
-            ClearBackground(RAYWHITE);
-            BeginMode2D(cam); {
-                drawPlayer(&player);
-                drawPlatforms(platforms, platformCount);
-            } EndMode2D();
-
-            DrawText("Multi-Mini-Games", WINDOW_WIDTH / 2 - MeasureText("Multi-Mini-Games", 20) / 2, 20, 20, PURPLE);
-            drawSkinButton();
-            
-            if (isTextureMenuOpen) {
-                drawMenuTextures();
+            default: {
+                log_error("GameScene_Et");
             }
-        } EndDrawing();
+        }
+    }
+
+    if (tetrisGame != NULL) {
+        tetris_freeGame(&tetrisGame);
     }
 
     for (int i = 0; i < playerTextureCount; i++)
--- tetris/src/ui/board.c
+++ tetris/src/ui/board.c
@@ -6,8 +6,8 @@ void drawBoard(board_t board) {
 
     for (int y = 0; y < BOARD_HEIGHT; y++) {
         for (int x = 0; x < BOARD_WIDTH; x++) {
-            DrawRectangle((offsetX + x * CELL_SIZE) - 1, (offsetY + y * CELL_SIZE) - 1, CELL_SIZE + 2, CELL_SIZE + 2, BOARD_GRID_COLOR);
-            DrawRectangle(offsetX + x * CELL_SIZE, offsetY + y * CELL_SIZE, CELL_SIZE, CELL_SIZE, board[y][x]);
+            DrawRectangleLines(offsetX + x * CELL_SIZE, offsetY + y * CELL_SIZE, CELL_SIZE, CELL_SIZE, BOARD_GRID_COLOR);
+            DrawRectangle     (offsetX + x * CELL_SIZE, offsetY + y * CELL_SIZE, CELL_SIZE-2, CELL_SIZE-2, board[y][x]);
         }
     }
 }
