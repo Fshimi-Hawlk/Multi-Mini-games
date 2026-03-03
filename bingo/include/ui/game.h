0d252c3d13f0398769df6c79f6d8dc231b109dbf
Fshimi Hawlk <fshimi.hawlk@gmail.com>
2026-03-03 20:49:21 +0100

    - feat: Implemented basic logic:   - bingo/game over detection   - random ball selection   - player board's square ticking when right number-colum   - player penality when missclicking too much a square with visual warning   - board rendering

---
+++ bingo/include/ui/game.h
@@ -0,0 +1,26 @@
+/**
+    @file ui/game.h
+    @author Fshimi-Hawlk
+    @date 2026-02-08              // Creation date - never touch this one again
+    @date 2026-02-23              // Last time someone meaningfully changed this file (update only when needed)
+    @brief One clear sentence that tells what this file is actually for.
+
+    If the file needs more context than fits in @brief, write 2-5 lines here.
+    @note Put warnings, important limitations, "we know it's ugly but...", or future plans here
+
+    // Try to align the `for` for better readability
+    // Try to keep the same order of the includes
+    Use @see `path/to/related/file.h` when this file depends heavily on another one.
+*/
+
+#ifndef UI_GAME_H
+#define UI_GAME_H
+
+/**
+    @brief Draws all UI elements for the current scene.
+
+    TODO
+*/
+void drawUI(void);
+
+#endif // UI_GAME_H
---
+++ bingo/src/setups/app.c
@@ -0,0 +1,64 @@
+/**
+    @file app.c
+    @author Fshimi Hawlk
+    @date 2026-01-07
+    @brief Application setup and teardown.
+*/
+
+#include "utils/common.h"
+#include "utils/globals.h"
+
+#include "setups/app.h"
+#include "setups/game.h"
+
+bool initFonts(void) {
+    u64 fontSize = 8;
+
+    bool allFontLoaded = true;
+
+    for (u64 fontId = 0; fontId < _fontSizeCount; fontId++) {
+        fonts[fontId] = LoadFontEx("../assets/fonts/Nunito/Nunito-Black.ttf", fontSize, NULL, 0);
+        if (!IsFontValid(fonts[fontId])) {
+            log_warn("Font %zu (%d) wasn't proprely loaded", fontId, fontSize);
+            allFontLoaded = false;
+        }
+
+        fontSize += 2;
+    }
+
+    return allFontLoaded;
+}
+
+void freeFonts(void) {
+    for (u64 fontId = 0; fontId < _fontSizeCount; fontId++) {
+        UnloadFont(fonts[fontId]);
+    }
+}
+
+bool initApp(void) {
+    srand(time(NULL));
+    SetTraceLogLevel(LOG_WARNING);
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(60);
+
+#ifdef LOGGER_H
+    init_logger();
+#endif
+
+    // if (!initFonts()) {
+    //     log_warn("Couldn't initialize every fonts");
+    // };
+
+    initGame();
+
+    return true;
+}
+
+void freeApp(void) {
+    // arena_free(&globalArena);
+    // arena_free(&tempArena);
+
+    freeFonts();
+
+    CloseWindow();
+}
