fe809839867e751cd65112db45fd269f57c85da7
Fshimi Hawlk <fshimi.hawlk@gmail.com>
2026-03-03 20:49:21 +0100

    - feat: Implemented basic logic:   - bingo/game over detection   - random ball selection   - player board's square ticking when right number-colum   - player penality when missclicking too much a square with visual warning   - board rendering

---
+++ bingo/include/setups/game.h
@@ -0,0 +1,20 @@
+/**
+    @file game.h (setups)
+    @author Fshimi Hawlk
+    @date 2026-01-07
+    @brief Full game state initialization.
+*/
+
+#ifndef SETUPS_GAME_H
+#define SETUPS_GAME_H
+
+#include "utils/userTypes.h"
+
+/**
+    @brief Initializes the game: prefabs, board, and state.
+
+    Sets up board, fills prefab bags, shuffles initial slots.
+*/
+bool initGame(void);
+
+#endif // SETUPS_GAME_H
